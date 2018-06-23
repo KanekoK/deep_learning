496045b416c4f7a6d983d0e8ef08b0dae8fc68ff
Kenya <b1r2d3q4@gmail.com>
2018-06-23 14:51:58 +0900

    .

--- logic_operation.py
+++ logic_operation.py
@@ -1,4 +1,14 @@
 import numpy as np
+import matplotlib.pylab as plt
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)
+plt.show()
 
 def AND(x1, x2):
     x = np.array([x1, x2])
@@ -35,5 +45,17 @@ def XOR(x1, x2):
     s2 = OR(x1, x2)
     y = AND(s1, s2)
     return y
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0, x)
+
+
 
 
