b6314e56988c2a3fdf6b515719c8db00f25a6a9a
Kenya <b1r2d3q4@gmail.com>
2018-06-23 15:45:28 +0900

    3層ニューラルネットワーク

--- logic_operation.py
+++ logic_operation.py
@@ -1,15 +1,6 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-x = np.arange(-5.0, 5.0, 0.1)
-y = sigmoid(x)
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1)
-plt.show()
-
 def AND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
