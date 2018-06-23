b46cad45213013b3541d780ed18e22850375b847
Kenya <b1r2d3q4@gmail.com>
2018-06-23 16:11:16 +0900

    .

--- logic_operation.py
+++ logic_operation.py
@@ -47,6 +47,15 @@ def sigmoid(x):
 def relu(x):
     return np.maximum(0, x)
 
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c) # オーバーフロー対策
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+
 
 
 
